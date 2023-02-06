a95c5d422a226c051859ba32a0e4eba2f60ba9d2
IvicaNeskoski <ivica.neskoski@gmail.com>
2023-02-06 19:55:10 +0100

    c11-reux-fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { CombinedReducers, combineReducers } from "redux";
+import { combineReducers } from "redux";
 import HelloReducer from "./HelloReducer";
 
 export default combineReducers({ HelloReducer });
