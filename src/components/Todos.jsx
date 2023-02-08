44fe92543747effee32e58fc736af6eeec78c404
IvicaNeskoski <ivica.neskoski@gmail.com>
2023-02-08 21:26:40 +0100

    cas13

---
+++ src/components/Todos.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { add, remove } from "../slices/todosSlice";
+
+export const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li
+          style={{ textDecoration: todo.completed ? "line-through" : "" }}
+          key={todo.id}
+        >
+          {todo.title}
+
+          <button
+            onClick={() => dispatch(remove(todo.id))}
+            style={{ marginLeft: "10px" }}
+          >
+            X
+          </button>
+        </li>
+      ))}
+
+      <button onClick={() => dispatch(add("New TODO Added"))}>Add</button>
+    </div>
+  );
+};
--- src/store.js
+++ src/store.js
@@ -2,8 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./slices/counterSlice";
 
+import todosReducer from "./slices/todosSlice";
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todos: todosReducer,
   },
 });
